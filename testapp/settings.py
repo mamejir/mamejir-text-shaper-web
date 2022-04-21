9e90b79236e540bc71efc8b88407fcad013dc5ec
mamejir <kage0129@gmail.com>
2022-04-22 00:51:22 +0900

    commit202204220051

--- testapp/settings.py
+++ testapp/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '.pythonanywhere.com', 'mamejir.pythonanywhere.com']
 
@@ -126,9 +125,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+<<<<<<< HEAD
 
 
 
+=======
+>>>>>>> parent of def3c45 (commit202204212252)
 DEPLOY = True
 if DEPLOY:
     # SECURE_HSTS_SECONDS = 60 # 無知にいじると危険なのでコメントアウト
